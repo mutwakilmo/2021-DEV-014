f4b31643c124d4fd0a424274a9c084911755818e
mutwakilmo <mutwakilmo@gmail.com>
2021-05-23 09:33:04 +0200

    update gameActivity

--- app/src/main/java/com/mutwaakil/a2021_dev_014/GameActivity.kt
+++ app/src/main/java/com/mutwaakil/a2021_dev_014/GameActivity.kt
@@ -2,10 +2,45 @@ package com.mutwaakil.a2021_dev_014
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
+
+
+    private lateinit var gameManager: GameManager
+    private lateinit var one: TextView
+    private lateinit var two: TextView
+    private lateinit var three: TextView
+    private lateinit var four: TextView
+    private lateinit var five: TextView
+    private lateinit var six: TextView
+    private lateinit var seven: TextView
+    private lateinit var eight: TextView
+    private lateinit var nine: TextView
+    private lateinit var startNewGameButton: Button
+    private lateinit var player1Points: TextView
+    private lateinit var player2Points: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+
+        gameManager = GameManager()
+
+        one = findViewById(R.id.one)
+        two = findViewById(R.id.two)
+        three = findViewById(R.id.three)
+        four = findViewById(R.id.four)
+        five = findViewById(R.id.five)
+        six = findViewById(R.id.six)
+        seven = findViewById(R.id.seven)
+        eight = findViewById(R.id.eight)
+        nine = findViewById(R.id.nine)
+        startNewGameButton = findViewById(R.id.start_new_game_button)
+        player1Points = findViewById(R.id.player_one_score)
+        player2Points = findViewById(R.id.player_two_score)
+        
     }
 }
