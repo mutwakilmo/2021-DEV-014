10ee585525906fbcc952894623870180b1619df3
mutwakilmo <mutwakilmo@gmail.com>
2021-05-23 08:07:27 +0200

    pseudo code TDD and Game Activity

---
+++ app/src/test/java/com/mutwaakil/a2021_dev_014/TicTacToe.kt
@@ -0,0 +1,19 @@
+package com.mutwaakil.a2021_dev_014
+
+/**
+ * Created by Mutwakil-Mo 🤩
+ * Android Engineer,
+ * Brussels
+ */
+class TicTacToe {
+
+//    X always goes first.
+//    Players cannot play on a played position.
+//    Players alternate placing X’s and O’s on the board until either:
+//
+  //    One player has three in a row, horizontally, vertically or diagonally
+  //    All nine squares are filled.
+//
+//    If a player is able to draw three X’s or three O’s in a row, that player wins.
+//    If all nine squares are filled and neither player has three in a row, the game is a draw
+}
