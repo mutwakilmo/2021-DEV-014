f82f6a00e13621af92473d22605f684a5b1b87e4
mutwakilmo <mutwakilmo@gmail.com>
2021-05-23 09:18:08 +0200

    update project

--- app/src/main/java/com/mutwaakil/a2021_dev_014/GameManager.kt
+++ app/src/main/java/com/mutwaakil/a2021_dev_014/GameManager.kt
@@ -6,4 +6,69 @@ package com.mutwaakil.a2021_dev_014
  * Brussels
  */
 class GameManager {
+
+    private var currentPlayer = 1
+    var player1Points = 0
+    var player2Points = 0
+
+    val currentPlayerMark: String
+        get() {
+            return if (currentPlayer == 1) "X" else "O"
+        }
+
+    private var state = arrayOf( // 2D Array
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0)
+    )
+
+
+    //Make move
+    fun makeMove(position: Position): WinningLine? {
+        state[position.row][position.column] = currentPlayer
+        val winningLine = hasGameEnded()
+
+        if (winningLine == null) {
+            currentPlayer = 3 - currentPlayer
+        } else {
+            when (currentPlayer) {
+                1 -> player1Points++
+                2 -> player2Points++
+            }
+        }
+
+        return winningLine
+    }
+
+    // reset
+    fun reset() {
+        state = arrayOf(
+            intArrayOf(0, 0, 0),
+            intArrayOf(0, 0, 0),
+            intArrayOf(0, 0, 0)
+        )
+        currentPlayer = 1
+    }
+
+    
+    private fun hasGameEnded(): WinningLine? {
+        if (state[0][0] == currentPlayer && state[0][1] == currentPlayer && state[0][2] == currentPlayer) {
+            return WinningLine.ROW_0
+        } else if (state[1][0] == currentPlayer && state[1][1] == currentPlayer && state[1][2] == currentPlayer) {
+            return WinningLine.ROW_1
+        } else if (state[2][0] == currentPlayer && state[2][1] == currentPlayer && state[2][2] == currentPlayer) {
+            return WinningLine.ROW_2
+        } else if (state[0][0] == currentPlayer && state[1][0] == currentPlayer && state[2][0] == currentPlayer) {
+            return WinningLine.COLUMN_0
+        } else if (state[0][1] == currentPlayer && state[1][1] == currentPlayer && state[2][1] == currentPlayer) {
+            return WinningLine.COLUMN_1
+        } else if (state[0][2] == currentPlayer && state[1][2] == currentPlayer && state[2][2] == currentPlayer) {
+            return WinningLine.COLUMN_2
+        } else if (state[0][0] == currentPlayer && state[1][1] == currentPlayer && state[2][2] == currentPlayer) {
+            return WinningLine.DIAGONAL_LEFT
+        } else if (state[0][2] == currentPlayer && state[1][1] == currentPlayer && state[2][0] == currentPlayer) {
+            return WinningLine.DIAGONAL_RIGHT
+        }
+        return null
+    }
 }
