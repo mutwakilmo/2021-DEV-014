675412bef13d1644f6ca0e89a1890b1ac7c16640
mutwakilmo <mutwakilmo@gmail.com>
2021-05-23 08:13:17 +0200

    update project

---
+++ app/src/main/java/com/mutwaakil/a2021_dev_014/Position.kt
@@ -0,0 +1,8 @@
+package com.mutwaakil.a2021_dev_014
+
+/**
+ * Created by Mutwakil-Mo 🤩
+ * Android Engineer,
+ * Brussels
+ */
+data class Position(val row: Int, val column: Int)
---
+++ app/src/main/java/com/mutwaakil/a2021_dev_014/WinningLine.kt
@@ -0,0 +1,17 @@
+package com.mutwaakil.a2021_dev_014
+
+/**
+ * Created by Mutwakil-Mo 🤩
+ * Android Engineer,
+ * Brussels
+ */
+enum class WinningLine {
+    ROW_0,
+    ROW_1,
+    ROW_2,
+    COLUMN_0,
+    COLUMN_1,
+    COLUMN_2,
+    DIAGONAL_LEFT,
+    DIAGONAL_RIGHT,
+}
